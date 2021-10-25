7118dad5fd603aa593d5d3fab8eaf17d9028df37
AndreyNikolaich <82803423+AndreyNikolaich@users.noreply.github.com>
2021-10-25 17:41:39 +0300

    создан тест UI

--- test_ui.py
+++ test_ui.py
@@ -1,2 +1,18 @@
-import d
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+
+def test_dokhodcivo():
+    wd = webdriver.Chrome()
+    link = "https://google.com/"
+    wd.get(link)
+    wd.find_element(By.CSS_SELECTOR, '[class="gLFyf gsfi"]').send_keys('dokhodchivo.ru')
+    wd.find_element(By.XPATH, '//div[3]/center/input').click()
+    #site = "https://dokhodchivo.ru/"
+    #for i in site:
+    wd.find_element(By.LINK_TEXT, 'https://dokhodchivo.ru/').click()
+
+
+
+
 
